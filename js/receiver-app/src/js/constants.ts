2665f34704407f8d25171703533aaf78061cb116
thomas kiljanczyk dev <thomas.kiljanczyk.dev@gmail.com>
2025-08-27 17:12:39 +0200

    Migrate receiver app to TypeScript

---
+++ js/receiver-app/src/js/constants.ts
@@ -0,0 +1,5 @@
+export const TEXT_NAMESPACE = 'urn:x-cast:example.cast.title';
+export const MOVE_NAMESPACE = 'urn:x-cast:example.cast.move';
+
+export const MOVE_STEP = 10;
+export const SQUARE_SIZE = 50;
